d4b3d8d8f6c95638945124ded8409d44ba91fa79
propelledahead <propelledahead@users.noreply.github.com>
2019-04-15 21:25:01 -0700

    Added User BLL, Model, and Repo
    Added some basic scaffholding for users

---
+++ Project/Playground/Playground/Authorities/Class.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Playground.Repositories;
+using Playground.Models;
+
+namespace Playground.Authorities
+{
+    public class UserAuthority
+    {
+        private UserRepository _UserRepository = null;
+
+        public UserAuthority(UserRepository oUserRepository) {
+            this._UserRepository = oUserRepository;
+        }
+
+        public void UserCreate(UserModel oUserModel) {
+            if (oUserModel == null) {
+                throw new ArgumentNullException("oUserModel");
+            }
+            this._UserRepository.UserCreate(oUserModel);
+        }
+
+    }
+}
---
+++ Project/Playground/Playground/Repositories/UserRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Playground.Models;
+
+namespace Playground.Repositories
+{
+    public abstract class UserRepository
+    {
+        public abstract bool UserCreate(UserModel oUserModel);
+        public abstract UserModel UserRead();
+        public abstract bool UserUpdate(UserModel oUserModel);
+        public abstract bool UserDelete(Guid oUserGuid);
+
+    }
+}
