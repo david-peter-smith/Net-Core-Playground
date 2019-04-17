3732348cf9c6172e9d93533d3b3957b4936879b3
propelledahead <propelledahead@users.noreply.github.com>
2019-04-17 01:59:53 -0700

    Renamed BLL and added database connector

---
+++ Project/Playground/Playground/Helpers/MSSQLHelper.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Collections;
+
+namespace Playground.Helpers
+{
+    public class MSSQLHelper
+    {
+        private SqlConnection _SqlConnection;
+        private string _parameter_prefix = "(@";
+        private string _parameter_suffix = ")";
+
+
+        public bool IsConnected() {
+            bool _result = false;
+            if (this._SqlConnection.State == System.Data.ConnectionState.Open) {
+                _result = true;
+            }
+            return _result;
+        }
+
+        public void Connect(string ConnetionString) {
+            if (!this.IsConnected()) {
+                this._SqlConnection = new SqlConnection(ConnetionString);
+                this._SqlConnection.Open();
+            }
+        }
+
+        public SqlDataReader Query(string SQL)
+        {
+            SqlDataReader _SqlDataReader = null;
+            SqlCommand _SqlCommand = new SqlCommand(SQL, this._SqlConnection);
+            _SqlDataReader = _SqlCommand.ExecuteReader();
+            return _SqlDataReader;
+        }
+
+        public SqlDataReader Query(string SQL, Hashtable ParametersList)
+        {
+            SqlDataReader _SqlDataReader = null;
+            SqlCommand _SqlCommand = new SqlCommand(SQL, this._SqlConnection);
+            foreach (string ParamName in ParametersList.Keys) {
+                if (!String.IsNullOrEmpty(ParamName) && !String.IsNullOrEmpty(ParamName.Trim()) )
+                {
+                    _SqlCommand.Parameters.AddWithValue(
+                        (this._parameter_prefix + ParamName + this._parameter_suffix)
+                        , ParametersList[ParamName]
+                    );
+                }
+            }
+            _SqlDataReader = _SqlCommand.ExecuteReader();
+            return _SqlDataReader;
+        }
+        public int Execute(string SQL)
+        {
+            int _rows_affected = 0;
+            SqlCommand _SqlCommand = new SqlCommand(SQL, this._SqlConnection);
+            _rows_affected = _SqlCommand.ExecuteNonQuery();
+            return _rows_affected;
+        }
+
+        public int Execute(string SQL, Hashtable ParametersList)
+        {
+            int _rows_affected = 0;
+            SqlCommand _SqlCommand = new SqlCommand(SQL, this._SqlConnection);
+            foreach (string ParamName in ParametersList.Keys)
+            {
+                if (!String.IsNullOrEmpty(ParamName) && !String.IsNullOrEmpty(ParamName.Trim()))
+                {
+                    _SqlCommand.Parameters.AddWithValue(
+                        (this._parameter_prefix + ParamName + this._parameter_suffix)
+                        , ParametersList[ParamName]
+                    );
+                }
+            }
+            _rows_affected = _SqlCommand.ExecuteNonQuery();
+            return _rows_affected;
+        }
+    }
+
+}
